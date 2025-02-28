1743bdd2b20e90f52ec1689a006bd6b566af1d20
AidanRJ1 <1aidanrj@gmail.com>
2025-02-28 09:43:57 +0000

    Add collision for CircleObjects, player and asteroid

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame, sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -14,6 +14,7 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
@@ -29,10 +30,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black") 
+        
         for obj in drawable:
             obj.draw(screen)
-        updatable.update(dt)
+        
+       
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
